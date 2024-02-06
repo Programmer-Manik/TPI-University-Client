70df193a81a89833b994c8e64ff53afa21b4fe34
Programmer-Manik <programmermanik1502083874@gmail.com>
2024-02-07 01:04:04 +0600

    Layout Component With Ant Design

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
+import MainLayout from "./components/layout/MainLayout"
 
 
 function App() {
-  return (
-    <>
-      <h1>manik</h1>
-    </>
-  )
+  return <MainLayout />
 }
 
 export default App
