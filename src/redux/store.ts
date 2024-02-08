153b7018f753e106d795f1c08341d958a0d1fd0d
Programmer-Manik <programmermanik1502083874@gmail.com>
2024-02-09 05:02:18 +0600

    (IMPORTANT) Set Cookies In The Browser

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,11 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit'
 import authSlice from './features/Auth/authSlice'
+import { baseApi } from './api/BaseApi'
 
 export const store = configureStore({
   reducer: {
+    [baseApi.reducerPath]:baseApi.reducer,
     auth: authSlice,
   },
-})
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(baseApi.middleware)
+});
+
 
 export type RootState = ReturnType<typeof store.getState>
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
