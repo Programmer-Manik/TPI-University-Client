1cc9b3a545a55c23161baec784123fdc13eafd1e
Programmer-Manik <programmermanik1502083874@gmail.com>
2024-02-29 00:17:18 +0600

    Handle Expired Refresh Token (Kick Out The User)

--- src/redux/api/BaseApi.ts
+++ src/redux/api/BaseApi.ts
@@ -14,14 +14,37 @@ const baseQuery = fetchBaseQuery({
   }
 });
 
-const baseQueryWithRefreshToken: BaseQueryFn<
-  FetchArgs,
-  BaseQueryApi,
-  DefinitionType
-> = async (args, api, extraOptions): Promise<any> => {
+const baseQueryWithRefreshToken = async (args, api, extraOptions): Promise<any> => {
   let result = await baseQuery(args, api, extraOptions);
 
-  console.log(result)
+  if (result?.error?.status === 401) {
+    //* Send Refresh
+    console.log('Sending refresh token');
+
+    const res = await fetch('http://localhost:5000/api/v1/auth/refresh-token', {
+      method: 'POST',
+      credentials: 'include',
+    });
+
+    const data = await res.json();
+
+    if (data?.data?.accessToken) {
+      const user = (api.getState() as RootState).auth.user;
+
+      api.dispatch(
+        setUser({
+          user,
+          token: data.data.accessToken,
+        })
+      );
+
+      result = await baseQuery(args, api, extraOptions);
+    } else {
+      api.dispatch(logout());
+    }
+  }
+
+  return result;
 };
 
 export const baseApi = createApi({
