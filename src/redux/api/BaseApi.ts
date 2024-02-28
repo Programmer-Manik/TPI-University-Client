0439cd5314b2d69067227a3316d60c4c4bc969e6
Programmer-Manik <programmermanik1502083874@gmail.com>
2024-02-28 23:47:41 +0600

    Handle Verify JWT Error And Setup A Custom Base Query

--- src/redux/api/BaseApi.ts
+++ src/redux/api/BaseApi.ts
@@ -1,22 +1,31 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { BaseQueryApi, BaseQueryFn, DefinitionType, FetchArgs, createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { RootState } from '../store';
+import { logout, setUser } from '../features/auth/authSlice';
 
 const baseQuery = fetchBaseQuery({
   baseUrl: 'http://localhost:5000/api/v1',
   credentials: 'include',
   prepareHeaders:(headers, {getState}) => {
     const token = (getState() as RootState).auth.token
-    
     if (token) {
       headers.set('authorization', `${token}`);
     }
-
     return headers;
   }
 });
 
+const baseQueryWithRefreshToken: BaseQueryFn<
+  FetchArgs,
+  BaseQueryApi,
+  DefinitionType
+> = async (args, api, extraOptions): Promise<any> => {
+  let result = await baseQuery(args, api, extraOptions);
+
+  console.log(result)
+};
+
 export const baseApi = createApi({
   reducerPath: 'baseApi',
-  baseQuery:baseQuery,
+  baseQuery:baseQueryWithRefreshToken,
   endpoints: () => ({}),
 });
